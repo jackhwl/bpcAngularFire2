7d38b0c410f633043b182cc9411fa8a11d9a3239
Jack Huang <jackhwl@hotmail.com>
2018-03-21 23:22:32 -0400

    change navbar collapse when small

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -45,36 +45,36 @@ export class UserService implements CanActivate {
         }
     }
 
-    login(loginEmail: string, loginPassword: string){
-        let vm = this;
-        this.au.auth.setPersistence(firebase.auth.Auth.Persistence.SESSION)
-        .then(function() {
-          // Existing and future Auth states are now persisted in the current
-          // session only. Closing the window would clear any existing state even
-          // if a user forgets to sign out.
-          // ...
-          // New sign-in will be persisted with session persistence.
-          vm.au.auth.signInWithEmailAndPassword(loginEmail, loginPassword)
-          .catch(function(error) {
-              alert('${error.message} Unable to login. Try Again!');
-          });
+    // login(loginEmail: string, loginPassword: string){
+    //     let vm = this;
+    //     this.au.auth.setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    //     .then(function() {
+    //       // Existing and future Auth states are now persisted in the current
+    //       // session only. Closing the window would clear any existing state even
+    //       // if a user forgets to sign out.
+    //       // ...
+    //       // New sign-in will be persisted with session persistence.
+    //       vm.au.auth.signInWithEmailAndPassword(loginEmail, loginPassword)
+    //       .catch(function(error) {
+    //           alert('${error.message} Unable to login. Try Again!');
+    //       });
         
-          //return firebase.auth().signInWithEmailAndPassword(email, password);
-        })
-        .catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          console.error(error);
-        });
-    }
+    //       //return firebase.auth().signInWithEmailAndPassword(email, password);
+    //     })
+    //     .catch(function(error) {
+    //       // Handle Errors here.
+    //       var errorCode = error.code;
+    //       var errorMessage = error.message;
+    //       console.error(error);
+    //     });
+    // }
 
-    logout() {
-        this.userLoggedIn = false;
-        this.au.auth.signOut().then(function(){
-            console.log('Logged Out');
-        }, function(error) {
-            alert('${error.message} Unabled to logout. Try again!');
-        });
-    }
+    // logout() {
+    //     this.userLoggedIn = false;
+    //     this.au.auth.signOut().then(function(){
+    //         console.log('Logged Out');
+    //     }, function(error) {
+    //         alert('${error.message} Unabled to logout. Try again!');
+    //     });
+    // }
 }
