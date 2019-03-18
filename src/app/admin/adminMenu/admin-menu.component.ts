64a9707c41b61ed48e4e2d2b43d047ecbdb4c63a
jackhwl <jackhwl@hotmail.com>
2019-03-17 22:22:09 -0400

    quill source partially

--- src/app/admin/adminMenu/admin-menu.component.ts
+++ src/app/admin/adminMenu/admin-menu.component.ts
@@ -17,17 +17,54 @@ export class AdminMenuComponent implements OnInit {
     theUser: string;
     headerChoice: string = '';
     misc: Misc;
+    editorStyle = {
+        height: '200px',
+        //width: '90vw',
+        backgroundColor: '#fff'
+    };
+    config = {
+        toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+            ['blockquote', 'code-block'],
+          
+            [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+            [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+            [{ 'direction': 'rtl' }],                         // text direction
+          
+            [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+          
+            [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+            [{ 'font': [] }],
+            [{ 'align': [] }],
+          
+            ['clean'],                                         // remove formatting button
+            ['link', 'image', 'video'],
+            ['showHtml'] //https://codepen.io/anon/pen/ZyEjrQ
+        ]
+    };
     //misc$: FirebaseObjectObservable<Misc>;
     
     constructor(  private menuAdminSVC: MenuAdminService, private menuSVC: MenuService, private userSVC: UserService, private router: Router ) {}
 
     ngOnInit(): void {
         this.editorForm = new FormGroup({
-            //editContent: new FormControl(),
-            editContent: new FormControl()
+            editContent: new FormControl(),
+            sourceContent: new FormControl()
         });
         this.menuSVC.setTopNav('admin', null);
         this.getMisc();
+
+        // var customButton = document.querySelector('.ql-showHtml');
+        // customButton.addEventListener('click', function() {
+        //     if (this.editorForm.controls.sourceContent.style.display === '') {
+        //         var html = this.editorForm.controls.sourceContent.value;
+        //         //self.quill.pasteHTML(html)
+        //     }
+        //     this.editorForm.controls.sourceContent.style.display = this.editorForm.controls.sourceContent.style.display === 'none' ? '' : 'none'
+        // });
     }
 
     chooseMode(mode: string){
@@ -52,4 +89,12 @@ export class AdminMenuComponent implements OnInit {
     //     this.userSVC.logout();
     //     this.router.navigate(['']);
     // }
+
+    maxLength(e) {
+        // console.log(e);
+        // if(e.editor.getLength() > 10) {
+        //     e.editor.deleteText(10, e.editor.getLength());
+        // }
+
+    }
  }
